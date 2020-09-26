18fd66bbc9c978b7793f899a68ef64e0864114f1
Willendless <317500141@qq.com>
2020-09-26 14:45:01 +0800

    feat: init skeleton

---
+++ jlox/README.md
@@ -0,0 +1,5 @@
+# jlox
+
+jlox is a tree-walker implementation of the lox programming language.
+
+## feature list
---
+++ jlox/src/Lox.java
@@ -0,0 +1,7 @@
+package jlox;
+
+public class Lox {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
