246ab074ec3b326ba6ebd4a89ae548f35f1ba8f1
Willendless <317500141@qq.com>
2020-10-14 16:34:11 +0800

    feat: finish basic scanner

--- jlox/src/jlox/Lox.java
+++ jlox/src/jlox/Lox.java
@@ -1,12 +1,20 @@
 package jlox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 public class Lox {
 
     static boolean hadError = false;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             usage();
-            return;
         } else if (args.length == 1) {
             runFile(args[0]);
         } else {
@@ -14,16 +22,47 @@ public class Lox {
         }
     }
 
-    private static void runFile(String path) {
-
-
-    }
-
-    private static void runPromt() {
-
-    }
-
     private static void usage() {
-        System.out.println("asdf");
+        System.out.println("Usage: jlox [script]");
     }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPromt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token t : tokens) {
+            System.out.println(t);
+        }
+
+    }
+
+    static void error(int line, String msg) {
+        report(line, "Error", msg);
+    }
+
+    private static void report(int line, String type, String msg) {
+        System.err.println("[line " + line + "] " + type + ": " + msg);
+        hadError = true;
+    }
+
 }
